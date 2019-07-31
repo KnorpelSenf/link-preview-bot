77e045ae0ef9a8e0efe240b83bab6432147eae65
Steffen <steffen@indigo-nms.eu>
2019-08-01 01:00:39 +0200

    Add help text if there are no links

--- main.py
+++ main.py
@@ -19,8 +19,11 @@ def webhook(request):
     # Telegram understands UTF-8, so encode text for unicode compatibility
     text = update.message.text.encode('utf-8').decode()
 
-    bot.send_message(chat_id=chat_id, text="There you go:")
     urls = get_links(text)
-    for url in urls:
-        bot.send_message(chat_id=chat_id, text=url,
-                         reply_to_message_id=msg_id, disable_web_page_preview=False)
+    if len(urls) == 0:
+        bot.send_message(chat_id=chat_id, text='No URLs found.',
+                         reply_to_message_id=msg_id)
+    else:
+        for url in urls:
+            bot.send_message(chat_id=chat_id, text=url,
+                             reply_to_message_id=msg_id, disable_web_page_preview=False)
