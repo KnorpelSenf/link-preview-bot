14139bb5cdc36e7985d48a4cb7e704250788de23
Steffen <steffen@indigo-nms.eu>
2019-07-31 18:23:42 +0200

    Editorconfig and more

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ global bot
 TOKEN = bot_token
 bot = telegram.Bot(token=TOKEN)
 
+
 def hello_http(request):
     """HTTP Cloud Function.
     Args:
@@ -33,55 +34,3 @@ def hello_http(request):
 
     response = get_response(text)
     bot.sendMessage(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
-
-    request_json = request.get_json(silent=True)
-    request_args = request.args
-
-    if request_json and 'name' in request_json:
-        name = request_json['name']
-    elif request_args and 'name' in request_args:
-        name = request_args['name']
-    else:
-        name = 'World'
-    return 'Hello {}!'.format(escape(name))
-
-#     # # note the threaded arg which allow
-#     # # your app to have more than one thread
-#     # app.run(threaded=True)
-
-
-# # # start the flask app
-# # app = Flask(__name__)
-# #
-# # @app.route('/{}'.format(TOKEN), methods=['POST'])
-# # def respond():
-# #     # retrieve the message in JSON and then transform it to Telegram object
-# #     update = telegram.Update.de_json(request.get_json(force=True), bot)
-# #     # get the chat_id to be able to respond to the same user
-# #     chat_id = update.message.chat.id
-# #     # get the message id to be able to reply to this specific message
-# #     msg_id = update.message.message_id
-# #     # Telegram understands UTF-8, so encode text for unicode compatibility
-# #     text = update.message.text.encode('utf-8').decode()
-# #     print("got text message :", text)
-# #     # here we call our super AI
-# #     response = get_response(text)
-# #     # now just send the message back
-# #     # notice how we specify the chat and the msg we reply to
-# #     bot.sendMessage(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
-# #     return 'ok'
-# #
-# # @app.route('/setwebhook', methods=['GET', 'POST'])
-# # def set_webhook():
-# #     # we use the bot object to link the bot to our app which live
-# #     # in the link provided by URL
-# #     s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
-# #     # something to let us know things work
-# #     if s:
-# #         return "webhook setup ok"
-# #     else:
-# #         return "webhook setup failed"
-# #
-# # @app.route('/')
-# # def index():
-# #     return '.'
