04865a65cf79b1bc8f422b43a3d30298cf7902a9
Steffen <steffen@indigo-nms.eu>
2019-08-01 00:42:21 +0200

    Redirect to desktop pages in some cases

--- telebot/mastermind.py
+++ telebot/mastermind.py
@@ -2,5 +2,21 @@ from urlextract import URLExtract
 
 extractor = URLExtract()
 
+
 def get_links(msg):
-    return extractor.find_urls(msg)
+    return [remove_mobile(url)
+            if url.startswith('http://') or url.startswith('https://')
+            else remove_mobile('http://' + url)
+            for url in extractor.gen_urls(msg)]
+
+
+def remove_mobile(url):
+    if url.startswith('http://m.'):
+        url = 'http://' + url[len('http://m.'):]
+    if url.startswith('https://m.'):
+        url = 'https://' + url[len('https://m.'):]
+    if '.m.' in url:
+        m_index = url.index('.m.')
+        if '/' not in url[len('https://')] or m_index < url.index('/', len('https://')):
+            url = url[:m_index] + url[m_index + 2:]
+    return url
