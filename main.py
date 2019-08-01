a37c963b299044af09f36daf3147ea049ff03dfc
Steffen <steffen@indigo-nms.eu>
2019-08-01 12:24:51 +0200

    Test

--- main.py
+++ main.py
@@ -19,35 +19,37 @@ def webhook(request):
     chat_id = message.chat.id
     msg_id = message.message_id
 
-    text = message.text or message.caption or ""
+    text = message.text or message.caption
 
-    print(text)
-
-    # Telegram understands UTF-8, so encode text for unicode compatibility
-    text8 = text.encode('utf-8').decode()
-    # MessageEntity objects refer to UTF-16 for offset and length
-    text16 = text.encode('utf-16').decode()
-
-    if text8 == '/help':
-        bot.send_message(chat_id=chat_id,
-                         text=("Send or forward me a message and I "
-                               "will respond with all URLs contained "
-                               "in it. I will try to give you the "
-                               "link preview whenever possible. This "
-                               "way, you can even read instant view "
-                               "articles if the original sender "
-                               "disabled this!"),
-                         reply_to_message_id=msg_id)
-        return
-
-    entities = message.entities or message.caption_entities or []
-
-    urls = get_links(text16, entities)
-
-    if len(urls) == 0:
-        bot.send_message(chat_id=chat_id, text='No links found.',
+    if text is None:
+        bot.send_message(chat_id=chat_id, text='No text in message.',
                          reply_to_message_id=msg_id)
     else:
-        for url in urls:
-            bot.send_message(chat_id=chat_id, text=url,
-                             reply_to_message_id=msg_id, disable_web_page_preview=False)
+        # Telegram understands UTF-8, so encode text for unicode compatibility
+        text8 = text.encode('utf-8').decode()
+        # MessageEntity objects refer to UTF-16 for offset and length
+        text16 = text # .encode('utf-16').decode()
+
+        if text8 == '/help':
+            bot.send_message(chat_id=chat_id,
+                             text=("Send or forward me a message and I "
+                                   "will respond with all URLs contained "
+                                   "in it. I will try to give you the "
+                                   "link preview whenever possible. This "
+                                   "way, you can even read instant view "
+                                   "articles if the original sender "
+                                   "disabled this!"),
+                             reply_to_message_id=msg_id)
+            return
+
+        entities = message.entities or message.caption_entities or []
+
+        urls = get_links(text16, entities)
+
+        if len(urls) == 0:
+            bot.send_message(chat_id=chat_id, text='No links found.',
+                             reply_to_message_id=msg_id)
+        else:
+            for url in urls:
+                bot.send_message(chat_id=chat_id, text=url,
+                                 reply_to_message_id=msg_id, disable_web_page_preview=False)
